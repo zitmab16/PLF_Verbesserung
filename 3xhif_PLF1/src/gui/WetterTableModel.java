602500ff3d6b698f22ab3590531cfb9972ccd34e
zitmab16 <zitmab16@htl-kaindorf.ac.at>
2018-11-05 10:17:13 +0100

    Implemented add function and Wetterstation class

---
+++ 3xhif_PLF1/src/gui/WetterTableModel.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author vizug
+ */
+public class WetterTableModel extends AbstractTableModel {
+    private ArrayList<Wetterstation> stations = new ArrayList();
+    private ArrayList<String> colnames = new ArrayList();
+
+    @Override
+    public int getRowCount() {
+        return stations.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colnames.size();
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Wetterstation w;
+        w=stations.get(rowIndex);
+        
+        return w;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+     return colnames.get(column);
+    }
+    public void add(Wetterstation w){
+        stations.add(w);
+        fireTableRowsInserted(stations.size()-1, stations.size()-1);
+    }
+    
+    
+}
---
+++ 3xhif_PLF1/src/gui/Wetterstation.java
@@ -0,0 +1,36 @@
+
+package gui;
+
+
+public class Wetterstation {
+    private String place;
+    private int sealvl;
+    private float temp;
+    private int humidity;
+
+    public Wetterstation(String place, int sealvl, float temp, int humidity) throws Exception {
+        this.place = place;
+        this.sealvl = sealvl;
+        setTemp(temp);
+        setHumidity(humidity);
+    }
+
+    public void setTemp(float temp) throws Exception{
+       
+        if(temp <-35 || temp>45){
+            throw new Exception("falsche Temperaturwerte!");
+        }
+        
+        this.temp = temp;
+    }
+
+    public void setHumidity(int humidity) throws Exception {
+        
+        if(humidity<0 ||humidity>100){
+            throw new Exception("falsche Luftfeuchtigkeitswerte");
+        }
+        this.humidity = humidity;
+    }
+    
+    
+}
